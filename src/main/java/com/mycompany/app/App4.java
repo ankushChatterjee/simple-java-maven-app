299109274ad4d34bbd6217a10a4803a29f39f558
Ankush Chatterjee <ankush.chatterjee@harness.io>
2025-02-10 13:18:44 +0530

    add new test 2

--- src/main/java/com/mycompany/app/App4.java
+++ src/main/java/com/mycompany/app/App4.java
@@ -3,11 +3,11 @@ package com.mycompany.app;
 /**
  * Hello world!
  */
-public class App3 {
+public class App4 {
 
-    private static final String MESSAGE = "Hey World!123";
+    private static final String MESSAGE = "Hey World!";
 
-    public App3() {}
+    public App4() {}
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
--- src/test/java/com/mycompany/app/App4Test.java
+++ src/test/java/com/mycompany/app/App4Test.java
@@ -7,19 +7,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * Unit test for simple App.
  */
-public class App3Test
+public class App4Test
 {
     @Test
     public void testAppConstructor() {
-        App app1 = new App();
-        App app2 = new App();
+        App4 app1 = new App4();
+        App4 app2 = new App4();
         assertEquals(app1.getMessage(), app2.getMessage());
     }
 
     @Test
     public void testAppMessage()
     {
-        App app = new App();
+        App4 app = new App4();
         assertEquals("Hello World!", app.getMessage());
     }
 }
