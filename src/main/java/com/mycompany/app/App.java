d98035248b4be49039b05cabcc73efd76a9d64c9
Ankush Chatterjee <ac.ankush15@gmail.com>
2025-08-13 16:32:37 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App {
 
     //DUMMY
-    private static final String MESSAGE = "Hello World!12";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
