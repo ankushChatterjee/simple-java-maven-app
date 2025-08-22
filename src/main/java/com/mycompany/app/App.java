63a244d033634b05ddb04e5585ca7780e6925790
Ankush Chatterjee <ac.ankush15@gmail.com>
2025-08-23 01:53:01 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App {
 
     // D1312
-    private static final String MESSAGE = "Hello World!1";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
